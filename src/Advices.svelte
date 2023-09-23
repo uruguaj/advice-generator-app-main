550f2627dbc2cf4ead57b6dc29c55f41a6840450
uruguaj <125591548+uruguaj@users.noreply.github.com>
2023-09-23 15:14:46 +0200

    better handling

--- src/Advices.svelte
+++ src/Advices.svelte
@@ -6,55 +6,36 @@
             res.json()
         )
     )
+
     const refetchData = ()=>{
         location.reload();
     }
 </script>
 
-{#if $queryResult.isLoading}
-    <main>
-        <div class="box">
-            <div class="in-box">
-                <span class="advice">Advice # Loading...</span>
-                <h1>„
-                    Loading...
-                    “</h1>
-                <img src="/images/pattern-divider-mobile.svg" alt="divider" class="mobile-divider"/>
-                <button on:click={refetchData}>
-                    <img src="/images/icon-dice.svg" alt="button-dice"/>
-                </button>
-            </div>
-        </div>
-        <div class="ad">
-            <span>If you want to support me check <a href="https://uruguaj.com">uruguaj.com</a></span>
-        </div>
-    </main>
-{:else if $queryResult.error}
-    <main>
-        <div class="box">
-            <div class="in-box">
-                <span class="advice">Advice #0.</span>
-                <h1>„
-                    Error has occurred! Try reload the page.
-                    “</h1>
-                <img src="/images/pattern-divider-mobile.svg" alt="divider" class="mobile-divider"/>
-                <button on:click={refetchData}>
-                    <img src="/images/icon-dice.svg" alt="button-dice"/>
-                </button>
-            </div>
-        </div>
-        <div class="ad">
-            <span>If you want to support me check <a href="https://uruguaj.com">uruguaj.com</a></span>
-        </div>
-    </main>
-{:else}
+
 <main>
     <div class="box">
         <div class="in-box">
+            {#if $queryResult.isLoading}
+            <span class="advice">Advice #Loading...</span>
+            {:else if $queryResult.error}
+                <span class="advice">Error has occurred!.</span>
+            {:else}
             <span class="advice">Advice #{$queryResult.data.slip.id}</span>
+            {/if}
+            {#if $queryResult.isLoading}
+                <h1>„
+                    Loading...
+                    “</h1>
+            {:else if $queryResult.error}
+                <h1>„
+                    Error has occurred! :(
+                    “</h1>
+            {:else}
             <h1>„
                 {$queryResult.data.slip.advice}
                 “</h1>
+            {/if}
             <img src="/images/pattern-divider-mobile.svg" alt="divider" class="mobile-divider"/>
             <button on:click={refetchData}>
                 <img src="/images/icon-dice.svg" alt="button-dice"/>
@@ -65,4 +46,3 @@
         <span>If you want to support me check <a href="https://uruguaj.com">uruguaj.com</a></span>
     </div>
 </main>
-{/if}
